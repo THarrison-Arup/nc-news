deb31b0599bc7376ae4119189e26f25e2983393c
Northcoders13 <northcoders13@Northcoders13s-MacBook-Pro.local>
2019-04-23 11:27:29 +0100

    links added to Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import './css/Header.css';
+import { Link } from '@reach/router'
 
 class Header extends Component {
   render() {
     return (
       <div className="Header">
-        Header 
+        <h2 className="header-Header" to="">NC News</h2>
+        <Link className="header-Home" to="/">Home</Link>
+        <Link className="header-Authors" to="/authors">Authors</Link>
+        <Link className="header-Articles" to="/articles">Articles</Link>
+        <Link className="header-Topics" to="/topics">Topics</Link>
+        <Link className="header-MyProfile" to="">My Profile</Link>
       </div>
     );
   }
--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './css/Logo.css';
 
 const Logo = () => {
   return (
