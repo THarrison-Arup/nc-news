cf3df15fe416a9050b419a1d1d7dafff74fa5f32
Northcoders13 <northcoders13@Northcoders13s-MacBook-Pro.local>
2019-04-24 15:52:37 +0100

    restored search results and user response windows in articles components

--- src/Components/ArticlesAdderResponse.jsx
+++ src/Components/ArticlesAdderResponse.jsx
@@ -2,10 +2,9 @@ import React from "react";
 
 const ArticlesAdderResponse = () => {
   return (
-    <main className="Articles-Add-Results">
-      <ul >
-        <h3>Server Response Window</h3>
-      </ul>
+    <main className="Articles-Add-Response">
+      <h3>Server Response Window</h3>
+      <ul className="Articles-Add-Results" />
     </main>
   );
 };
--- src/Components/ArticlesSearchResponse.jsx
+++ src/Components/ArticlesSearchResponse.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const ArticlesSearchResponse = () => {
   return (
-    <main className="Articles-Search-Results">
-      <ul >
-        <h3>Search Results</h3>
+    <main className="Articles-Search-Repsonse">
+      <h3>Search Results</h3>
+      <ul className="Articles-Search-Results">
         {/* <li>{article ? article.title : ""}</li> */}
       </ul>
     </main>
