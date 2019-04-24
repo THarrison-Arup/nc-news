114307d95acea534247b4fea4dcc1e8f3ecd8d6e
Northcoders13 <northcoders13@Northcoders13s-MacBook-Pro.local>
2019-04-24 15:47:43 +0100

    refactored to restore search and clear functionality to articles components

--- src/Components/ArticlesAdderForm.jsx
+++ src/Components/ArticlesAdderForm.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 
-const ArticlesAdderForm = ({}) => {
+const ArticlesAdderForm = ({
+  handleArticleAddSubmit,
+  handleArticleAddClear,
+  handleArticleAddInput,
+  addCriteria
+}) => {
+  const { addauthor, addtitle, addtopic } = addCriteria;
+
   return (
     <form className="Articles-Add-Form">
       <h3>Add an Article!</h3>
@@ -8,37 +15,37 @@ const ArticlesAdderForm = ({}) => {
         id="Articles-Add-Title"
         type="text"
         name="addtitle"
-        // value={addtitle}
-        // placeholder="Article Title..."
-        // onChange={event => handleArticleAddInput(event)}
+        value={addtitle}
+        placeholder="Article Title..."
+        onChange={event => handleArticleAddInput(event)}
       />
       <input
         id="Articles-Add-Author"
         type="text"
         name="addauthor"
-        // value={addauthor}
-        // placeholder="Article Author..."
-        // onChange={event => handleArticleAddInput(event)}
+        value={addauthor}
+        placeholder="Article Author..."
+        onChange={event => handleArticleAddInput(event)}
       />
       <input
         id="Articles-Add-Topic"
         type="text"
         name="addtopic"
-        // value={addtopic}
-        // placeholder="Article Topic..."
-        // onChange={event => handleArticleAddInput(event)}
+        value={addtopic}
+        placeholder="Article Topic..."
+        onChange={event => handleArticleAddInput(event)}
       />
       <button
         id="Articles-Add-Submit"
         type="submit"
-        // onClick={event => handleArticleAddSubmit(event)}
+        onClick={event => handleArticleAddSubmit(event)}
       >
         Add Article
       </button>
       <button
         id="Articles-Add-Clear"
         type="submit"
-        // onClick={event => handleArticleAddClear(event)}
+        onClick={event => handleArticleAddClear(event)}
       >
         Clear Article
       </button>
--- src/Components/ArticlesSearchForm.jsx
+++ src/Components/ArticlesSearchForm.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React from "react";
 
 const ArticlesSearchForm = ({
   handleArticleSearchSubmit,
   handleArticleSearchInput,
-  handleArticleSearchClear
+  handleArticleSearchClear,
+  searchCriteria
 }) => {
+  const { searchauthor, searchid, searchtitle } = searchCriteria;
 
   return (
     <form className="Articles-Search-Form">
@@ -13,37 +15,37 @@ const ArticlesSearchForm = ({
         id="Articles-Search-ID"
         name="searchid"
         type="text"
-        // value={searchid}
-        // placeholder="Article ID..."
-        // onChange={event => handleArticleSearchInput(event)}
+        value={searchid}
+        placeholder="Article ID..."
+        onChange={event => handleArticleSearchInput(event)}
       />
       <input
         id="Articles-Search-Title"
         name="searchtitle"
         type="text"
-        // value={searchtitle}
-        // placeholder="Article Title..."
-        // onChange={event => handleArticleSearchInput(event)}
+        value={searchtitle}
+        placeholder="Article Title..."
+        onChange={event => handleArticleSearchInput(event)}
       />
       <input
         id="Articles-Search-Author"
         name="searchauthor"
         type="text"
-        // value={searchauthor}
-        // placeholder="Article Author..."
-        // onChange={event => handleArticleSearchInput(event)}
+        value={searchauthor}
+        placeholder="Article Author..."
+        onChange={event => handleArticleSearchInput(event)}
       />
       <button
         id="Articles-Search-Submit"
         type="submit"
-        // onClick={event => handleArticleSearchSubmit(event)}
+        onClick={event => handleArticleSearchSubmit(event)}
       >
         Search
       </button>
       <button
         id="Articles-Search-Clear"
         type="submit"
-        // onClick={event => handleArticleSearchClear(event)}
+        onClick={event => handleArticleSearchClear(event)}
       >
         Clear Search
       </button>
