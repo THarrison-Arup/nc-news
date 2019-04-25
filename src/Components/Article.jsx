9158cfd12ebea4179e5690c64697eec8cae1274f
Northcoders13 <northcoders13@Northcoders13s-MacBook-Pro.local>
2019-04-25 09:56:33 +0100

    refactored Article.jsx from function component to class component

--- src/Components/Article.jsx
+++ src/Components/Article.jsx
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
 import * as api from "../api";
 import "./css/MainArticle.css";
 import ArticleInformation from "./ArticleInformation";
 import ArticleComments from "./ArticleComments";
 
-const Article = () => {
-  return (
-    <div className="Main-Article">
-      <h2>Article Title Goes Here!</h2>
-      <ArticleInformation />
-      <ArticleComments />
-    </div>
-  );
-};
+class Article extends Component {
+  state = {};
+  render() {
+    const {} = this.state;
+    const {} = this.props;
+
+    return (
+      <div className="Main-Article">
+        <h2>Article Title Goes Here!</h2>
+        <ArticleInformation />
+        <ArticleComments />
+      </div>
+    );
+  }
+}
 
 export default Article;
