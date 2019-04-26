20548cc03237b24686d53109f2f2c3c0b9186f41
Northcoders13 <northcoders13@Northcoders13s-MacBook-Pro.local>
2019-04-26 11:41:12 +0100

    added link to topic slug in Topics-Topic-Card

--- src/Components/TopicsWindow.jsx
+++ src/Components/TopicsWindow.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import TopicCardArticles from "./TopicCardArticles";
+import { Link } from '@reach/router';
 
 const TopicsWindow = props => {
   const { topics, articles } = props;
@@ -10,7 +11,11 @@ const TopicsWindow = props => {
       {topics.map(topic => {
         return (
           <li className="Topics-Topic-Card" key={topic.slug}>
-            <h3 className="Topics-Topic-Card-Slug">{topic.slug}</h3>
+            <h3
+              className="Topics-Topic-Card-Slug"
+              ><Link
+                to={`/topics/${topic.slug}`}
+                >{topic.slug}</Link></h3>
             <p className="Topics-Topic-Card-Description">{topic.description}</p>
             <TopicCardArticles articles={articles} topic={topic.slug} />
           </li>
