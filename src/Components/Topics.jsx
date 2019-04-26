7a00b634276cb6c265485758524d70755da8a737
Northcoders13 <northcoders13@Northcoders13s-MacBook-Pro.local>
2019-04-26 09:07:52 +0100

    Topics-Window component created

--- src/Components/Topics.jsx
+++ src/Components/Topics.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import "./css/MainTopics.css"
 // import * as api from "../api";
+import TopicsWindow from './TopicsWindow';
 
 const Topics = () => {
   return (
     <div className="Main-Topics">
-      A load of Topics
+      <TopicsWindow className="Topics-Window"/>
     </div>
   );
 };
---
+++ src/Components/TopicsWindow.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TopicsWindow = () => {
+  return (
+    <div>
+      A window for topics!
+    </div>
+  );
+};
+
+export default TopicsWindow;
