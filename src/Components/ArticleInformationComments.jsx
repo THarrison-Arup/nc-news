b620e56ee0edd90e15e828ee40086db462883eb1
Northcoders13 <northcoders13@Northcoders13s-MacBook-Pro.local>
2019-04-26 12:08:48 +0100

    added single select option functionality to radio buttons in Article-Information-Comments component

--- src/Components/ArticleInformationComments.jsx
+++ src/Components/ArticleInformationComments.jsx
@@ -1,37 +1,69 @@
-import React from "react";
+import React, {Component} from "react";
 
-const ArticleInformationComments = props => {
-  const { comments } = props;
-  return (
-    <div className="Article-Information-Comments">
-      <h3 className="Article-Information-Comments-Title">Comments</h3>
-      <form className="Article-Information-Comments-Sort">
-        <div className="Article-Information-Comments-Radio">
-          <label>
-            <input type="radio" value="Date Created" checked={true} />
-            Date Created
-          </label>
-        </div>
-        <div className="Article-Information-Comments-Radio">
-          <label>
-            <input type="radio" value="Comment Count" />
-            Comment Count
-          </label>
-        </div>
-        <div className="Article-Information-Comments-Radio">
-          <label>
-            <input type="radio" value="Votes" />
-            Votes
-          </label>
-        </div>
-      </form>
-      <ul className="Article-Information-Comments-List">
-        {comments.map(comment => {
-          return <li>{comment.body}</li>;
-        })}
-      </ul>
-    </div>
-  );
-};
+class ArticleInformationComments extends Component {
+  state = {
+    selectedOption: "DateCreated"
+  };
+  render() {
+    const { comments } = this.props;
+
+    const { selectedOption } = this.state;
+    return (
+      <div className="Article-Information-Comments">
+        <h3 className="Article-Information-Comments-Title">Comments</h3>
+        <form className="Article-Information-Comments-Sort">
+          <div className="Article-Information-Comments-Radio">
+            <label>
+              <input
+                type="radio"
+                value="DateCreated"
+                checked={selectedOption === 'DateCreated'}
+                onChange={this.handleChange}
+              />
+              Date Created
+            </label>
+          </div>
+          <div className="Article-Information-Comments-Radio">
+            <label>
+              <input
+                type="radio"
+                value="CommentCount"
+                checked={selectedOption === 'CommentCount'}
+                onChange={this.handleChange}
+                />
+              Comment Count
+            </label>
+          </div>
+          <div className="Article-Information-Comments-Radio">
+            <label>
+              <input
+                type="radio"
+                value="Votes"
+                checked={selectedOption === 'Votes'}
+                onChange={this.handleChange}
+                />
+              Votes
+            </label>
+          </div>
+        </form>
+        <ul className="Article-Information-Comments-List">
+          {comments.map(comment => {
+            return <li>{comment.body}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  }
+  selectedOption = () => {
+
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      selectedOption: event.target.value
+    });
+  };
+
+}
 
 export default ArticleInformationComments;
