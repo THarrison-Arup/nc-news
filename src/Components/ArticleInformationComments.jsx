d5369a167180d051edb81d71d394f6fcf106692c
Northcoders13 <northcoders13@Northcoders13s-MacBook-Pro.local>
2019-04-26 11:55:38 +0100

    added radio buttons to Article-Information-Comments component for sort-by functionality

--- src/Components/ArticleInformationComments.jsx
+++ src/Components/ArticleInformationComments.jsx
@@ -1,14 +1,36 @@
-import React from 'react';
+import React from "react";
 
-const ArticleInformationComments = (props) => {
-  const {comments} = props;
+const ArticleInformationComments = props => {
+  const { comments } = props;
   return (
-    <ul className="Article-Information-Comments">
-    <h3>Comments</h3>
-      {comments.map(comment => {
-        return <li>{comment.body}</li>
-      })}
-    </ul>
+    <div className="Article-Information-Comments">
+      <h3 className="Article-Information-Comments-Title">Comments</h3>
+      <form className="Article-Information-Comments-Sort">
+        <div className="Article-Information-Comments-Radio">
+          <label>
+            <input type="radio" value="Date Created" checked={true} />
+            Date Created
+          </label>
+        </div>
+        <div className="Article-Information-Comments-Radio">
+          <label>
+            <input type="radio" value="Comment Count" />
+            Comment Count
+          </label>
+        </div>
+        <div className="Article-Information-Comments-Radio">
+          <label>
+            <input type="radio" value="Votes" />
+            Votes
+          </label>
+        </div>
+      </form>
+      <ul className="Article-Information-Comments-List">
+        {comments.map(comment => {
+          return <li>{comment.body}</li>;
+        })}
+      </ul>
+    </div>
   );
 };
 
