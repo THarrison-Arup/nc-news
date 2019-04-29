6d3c7f29fbf245ae81b237ec6171f051448237d4
Northcoders13 <northcoders13@Northcoders13s-MacBook-Pro.local>
2019-04-29 16:51:40 +0100

    added array index to Nav-Articles and Nav-Topics list item keys; also added vote count to Top Articles component list items

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -8,14 +8,16 @@ const Nav = ({ topics, articles }) => {
     <div className="Nav">
       <ol className="Nav-Home-Articles">
         <h4>Top Articles</h4>
-        {articles.map(article => (
-          <li key={article.article_id}><Link to={`/articles/${article.article_id}`}>{article.title}</Link> by {article.author}<br/><br/></li>
+        {articles.map((article, ind) => (
+          <li key={ind}><Link to={`/articles/${article.article_id}`}>{`${ind+1}. ${article.title}`}</Link>
+          <br/> by {article.author}<br/>
+          Votes = {article.votes}<br/><br/></li>
         ))}
       </ol>
       <ol className="Nav-Home-Topics">
         <h4>Hot Topics</h4>
-        {topics.map(topic => (
-          <li key={topic.slug}><Link to={`/topics/${topic.slug}`}>{topic.slug}</Link></li>
+        {topics.map((topic, ind) => (
+          <li key={ind}><Link to={`/topics/${topic.slug}`}>{`${ind+1}. ${topic.slug}`}</Link></li>
         ))}
       </ol>
     </div>
