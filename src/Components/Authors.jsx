1fa52492a0dab7cfa4b924c8b8bf9fa2efdf298f
Northcoders13 <northcoders13@Northcoders13s-MacBook-Pro.local>
2019-04-29 16:17:46 +0100

    added link in Authors page to redirect to home.

--- src/Components/Authors.jsx
+++ src/Components/Authors.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import "./css/Main.css";
+import {Link} from '@reach/router';
 // import * as api from "../api";
 
 const Authors = () => {
   return (
     <div>
-      <h2>Coming Soon!</h2>
+      <h2>Coming Soon!
+
+        Click <Link to="/">HERE</Link> to go to Home.
+      </h2>
     </div>
   );
 };
