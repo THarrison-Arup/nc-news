98615686473b02c5779e61cd08aff185afd1bc5b
Northcoders13 <northcoders13@Northcoders13s-MacBook-Pro.local>
2019-04-30 09:47:52 +0100

    added sort function to Articles-Search-Results component to sort search results based on sort_by criteria

--- src/Components/ArticlesSearchResponse.jsx
+++ src/Components/ArticlesSearchResponse.jsx
@@ -1,15 +1,45 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { Link } from "@reach/router";
 
 class ArticlesSearchResponse extends Component {
   state = {
-    selectedOption: "DateCreated"
+    selectedOption: "created_at"
   };
 
   render() {
     const { articleData, handleSortInput } = this.props;
 
     const { selectedOption } = this.state;
+
+    function articleSort(a, b) {
+      if (selectedOption === "created_at") {
+        if (a.created_at < b.created_at) {
+          return 1;
+        }
+        if (a.created_at > b.created_at) {
+          return -1;
+        }
+        return 0;
+      }
+      if (selectedOption === "comment_count") {
+        if (a.comment_count < b.comment_count) {
+          return 1;
+        }
+        if (a.comment_count > b.comment_count) {
+          return -1;
+        }
+        return 0;
+      }
+      if (selectedOption === "votes") {
+        if (a.votes < b.votes) {
+          return 1;
+        }
+        if (a.votes > b.votes) {
+          return -1;
+        }
+        return 0;
+      }
+    }
 
     return (
       <main className="Articles-Search-Repsonse">
@@ -23,7 +53,7 @@ class ArticlesSearchResponse extends Component {
                 checked={selectedOption === "created_at"}
                 onChange={this.handleChange}
               />
-                Date Created
+              Date Created
             </label>
           </div>
           <div className="Article-Information-Results-Radio">
@@ -34,7 +64,7 @@ class ArticlesSearchResponse extends Component {
                 checked={selectedOption === "comment_count"}
                 onChange={this.handleChange}
               />
-                Comment Count
+              Comment Count
             </label>
           </div>
           <div className="Article-Information-Results-Radio">
@@ -45,34 +75,33 @@ class ArticlesSearchResponse extends Component {
                 checked={selectedOption === "votes"}
                 onChange={this.handleChange}
               />
-                Votes
+              Votes
             </label>
           </div>
         </form>
         <ul className="Articles-Search-Results">
-          {articleData.map((article, ind) => {
-            return (
-              article ?
+          {articleData.sort(articleSort).map((article, ind) => {
+            return article ? (
               <li key={ind}>
-                {`${ind+1}. `}
-                <Link to={`${article.article_id}`}>
-                  {`${article.title}`}
-                </Link>
-                <br/> by {`${article.author}`}
-                <br/> Votes: {`${article.votes}`} Comments: {`${article.comment_count}`} Created: {`${article.created_at}`.slice(0,10)}<br/><br/>
+                {`${ind + 1}. `}
+                <Link to={`${article.article_id}`}>{`${article.title}`}</Link>
+                <br /> by {`${article.author}`}
+                <br /> Votes: {`${article.votes}`} Comments:{" "}
+                {`${article.comment_count}`} Created:{" "}
+                {`${article.created_at}`.slice(0, 10)}
+                <br />
+                <br />
               </li>
-                 : 
-                <li>
-                  ""
-              </li>
-                )}
-            )}
+            ) : (
+              <li>""</li>
+            );
+          })}
         </ul>
       </main>
     );
-  };
+  }
 
-  handleChange = (event) => {
+  handleChange = event => {
     this.setState({
       selectedOption: event.target.value
     });
